42f75927e6a274321a72e6f6b4ee236e8a5b808d
Steve Higham (iMac) <steve@sjlt.co.uk>
2023-06-06 19:54:06 +0100

    Various config and app changes.

--- ch04/rq04-gallery-class-v1/src/App.js
+++ ch04/rq04-gallery-class-v1/src/App.js
@@ -1,43 +1,33 @@
-import { Component } from "react";
-
-class App extends Component {
-  render() {
-    return (
-      <main>
-        <h1>Animals</h1>
-        <Gallery />
-      </main>
-    );
-  }
+function Image ({index, title})
+{
+  return (
+    <figure style={{margin: "5px"}}>
+      <img
+        src={`//picsum.photos/id/${index}/150/150/`}
+        alt={title}
+      />
+      <figcaption>
+        <h3>Species: {title}</h3>
+      </figcaption>
+    </figure>
+  );
 }
 
-class Gallery extends Component {
-  render() {
-    return (
-      <section style={{ display: "flex" }}>
-        <Image index="1003" title="Deer" />
-        <Image index="1020" title="Bear" />
-        <Image index="1024" title="Vulture" />
-        <Image index="1084" title="Walrus" />
-      </section>
-    );
-  }
+function Gallery ()
+{
+  return (
+    <section style={{display: "flex"}}>
+      <Image index="1003" title="Deer"/>
+      <Image index="1020" title="Bear"/>
+      <Image index="1024" title="Vulture"/>
+      <Image index="1084" title="Walrus"/>
+    </section>
+  );
 }
 
-class Image extends Component {
-  render() {
-    return (
-      <figure style={{ margin: "5px" }}>
-        <img
-          src={`//picsum.photos/id/${this.props.index}/150/150/`}
-          alt={this.props.title}
-        />
-        <figcaption>
-          <h3>Species: {this.props.title}</h3>
-        </figcaption>
-      </figure>
-    );
-  }
+function App ()
+{
+  return <main><h1>Animals</h1><Gallery /></main>;
 }
 
 export default App;
