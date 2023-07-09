e83a3dc82a5b9ff6e9f9f8694cdfc9c52786b623
Steve Higham (iMac) <steve@sjlt.co.uk>
2023-07-09 19:42:50 +0100

    Various config and app changes.

--- ch05/rq05-nice-todo/src/App.js
+++ ch05/rq05-nice-todo/src/App.js
@@ -1,8 +1,27 @@
-import { useState } from "react";
-function markDone(list, index) {
-  return list.map((item, i) => (i === index ? { ...item, done: true } : item));
+import {useState} from "react";
+
+/**
+ * Returns a copy of with the specified index item marked as done
+ * @param list array of items where each item has index, done properties
+ * @param index Specifies the index value to mark as done
+ * @returns {*} The new list with the appropriate index item marked as done
+ */
+function markDone (list, index)
+{
+  return list.map ((item, i) => (i === index ? {...item, done: true} : item));
 }
-function FilterButton({ current, flag, setFilter, children }) {
+
+/**
+ * Renders the specified filter button
+ * @param current   Boolean - true iff hideDone is true
+ * @param flag      Boolean - false for showAll button, true for hideDone button
+ * @param setFilter Function - toggle the filter values
+ * @param children  Child elements - in this case 'Show all' or 'Hide done'
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function FilterButton ({current, flag, setFilter, children})
+{
   const style = {
     border: "1px solid dimgray",
     background: current === flag ? "dimgray" : "transparent",
@@ -10,12 +29,22 @@ function FilterButton({ current, flag, setFilter, children }) {
     padding: "4px 10px",
   };
   return (
-    <button style={style} onClick={() => setFilter(flag)}>
+    <button name = 'FilterButton' style = {style} onClick = {() => setFilter (flag)}>
       {children}
     </button>
   );
 }
-function Task({ task, done, markDone }) {
+
+/**
+ * Renders the specified task button followed by the task content
+ * @param   task      HTML Task content e.g. 'Feed the cat'
+ * @param   done      Boolean
+ * @param   markDone  Function - Update state to mark task as done
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Task ({task, done, markDone})
+{
   const paragraphStyle = {
     color: done ? "gray" : "black",
     borderLeft: "2px solid",
@@ -27,45 +56,53 @@ function Task({ task, done, markDone }) {
     color: "inherit",
   };
   return (
-    <p style={paragraphStyle}>
-      <button style={buttonStyle} onClick={done ? null : markDone}>
+    <p style = {paragraphStyle}>
+      <button name = 'TaskButton' style = {buttonStyle}
+              onClick = {done ? null : markDone}>
         {done ? "✓ " : "◯ "}
       </button>
       {task}
     </p>
   );
 }
-function TodoApplication({ initialList }) {
-  const [todos, setTodos] = useState(initialList);
-  const [hideDone, setHideDone] = useState(false);
-  const filteredTodos = hideDone ? todos.filter(({ done }) => !done) : todos;
+
+function TodoApplication ({initialList})
+{
+  const [todos, setTodos] = useState (initialList);
+  const [hideDone, setHideDone] = useState (false);
+  const filteredTodos = hideDone ? todos.filter (({done}) => !done) : todos;
   return (
     <main>
-      <div style={{ display: "flex" }}>
-        <FilterButton current={hideDone} flag={false} setFilter={setHideDone}>
+      <div style = {{display: "flex"}}>
+        <FilterButton current = {hideDone} flag = {false} setFilter = {setHideDone}>
           Show all
         </FilterButton>
-        <FilterButton current={hideDone} flag={true} setFilter={setHideDone}>
+        <FilterButton current = {hideDone} flag = {true} setFilter = {setHideDone}>
           Hide done
         </FilterButton>
       </div>
-      {filteredTodos.map((todo, index) => (
+      {filteredTodos.map ((todo) => (
         <Task
-          key={todo.task}
-          task={todo.task}
-          done={todo.done}
-          markDone={() => setTodos((value) => markDone(value, todo.index))}
+          key = {todo.task}
+          task = {todo.task}
+          done = {todo.done}
+          markDone = {() =>
+            setTodos ((value) => markDone (value, todo.index))
+          }
         />
       ))}
     </main>
   );
 }
-function App() {
+
+function App ()
+{
   const items = [
-    { task: "Feed the plants", done: false, index: 0 },
-    { task: "Water the dishes", done: false, index: 1 },
-    { task: "Clean the cat", done: false, index: 2 },
+    {task: "Feed the plants", done: false, index: 0},
+    {task: "Water the dishes", done: false, index: 1},
+    {task: "Clean the cat", done: false, index: 2},
   ];
-  return <TodoApplication initialList={items} />;
+  return <TodoApplication initialList = {items}/>;
 }
+
 export default App;
--- ch06/rq06-remote-dropdown/src/App.js
+++ ch06/rq06-remote-dropdown/src/App.js
@@ -1,23 +1,26 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
-function RemoteDropdown() {
-  const [options, setOptions] = useState([]);
-  useEffect(() => {
-    fetch("//www.swapi.tech/api/people")
-      .then((res) => res.json())
-      .then((data) => setOptions(data.results.map(({ name }) => name)));
+function RemoteDropdown ()
+{
+  const [options, setOptions] = useState ([]);
+  useEffect (() =>
+  {
+    fetch ("//www.swapi.tech/api/people")
+      .then ((res) => res.json ())
+      .then ((data) => setOptions (data.results.map (({name}) => name)));
   }, []);
   return (
     <select>
-      {options.map((option) => (
+      {options.map ((option) => (
         <option key={option}>{option}</option>
       ))}
     </select>
   );
 }
 
-function App() {
-  return <RemoteDropdown />;
+function App ()
+{
+  return <RemoteDropdown/>;
 }
 
 export default App;
